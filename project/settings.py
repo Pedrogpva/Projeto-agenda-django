305dc4867d5222eba543abeadaffece0049e3924
Pedro Henrique <henriquegpva@gmail.com>
2025-04-26 09:44:46 -0300

    Criado a tabela no index com os contatos

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,11 @@ MEDIA_ROO = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
+
