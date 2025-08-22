f5a4a54bde08a734eefbea9323935f5fccea35c2
Pedro Henrique <henriquegpva@gmail.comcls>
2025-08-22 09:14:28 -0300

    Inserindo mesagens de erros no form

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,14 @@ from contact import views
 
 app_name = 'contact'
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
+   
     path('', views.index, name='index'),
+    path('search', views.search, name='search'),
+
+    #CRUD
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create')
+
+   
     
 ]
---
+++ contact/views/contact_forms.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from contact.models import Contact
+from contact.forms import ContactForm
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(request, 'contact/create.html', context)
+    context = {
+            'form': ContactForm()
+        }
+    print(request.method)
+    return render(request, 'contact/create.html', context)
